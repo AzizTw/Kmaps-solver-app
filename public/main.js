5c20f0454b37a405987a6e32ec0ff5d71821117a
saher <msaher.shair@gmail.com>
2022-12-01 16:52:18 +0300

    parse kmap into a json input

--- public/main.js
+++ public/main.js
@@ -1,16 +1,48 @@
 const VALUES = ['&nbsp;', '1', 'X'];
 const LENGTH = 3;
 
-function nextValue(cell) { // here cell is a div
-    let val = cell.innerHTML;
-    cell.innerHTML = VALUES[(VALUES.indexOf(val)+1) % LENGTH]
+function nextValue(val) {
+    return VALUES[(VALUES.indexOf(val)+1) % LENGTH]
+}
+
+// Given an array of values, it returns an object containing:
+// - number of variables
+// - minterms
+// - dontcares
+function getKmapInput(arr) {
+    let mins = [];
+    let dcs = []; // dont_cares
+    let n = 4; // TODO: hardcoded to 4 for now
+
+    let v;
+    for (let i = 0; i < arr.length; i++) {
+        v = arr[i];
+        if (v === '1')
+            mins.push(i);
+
+        else if (v === 'X')
+            dcs.push(i);
+    }
+
+    return {mins, dcs, n};
+}
+
+function solve(cells) { // cells ia nodelist of divs
+    let vals = Array.from(cells).map((c) => c.innerHTML);
+    let input = getKmapInput(vals);
+    console.log(input)
 }
 
 function main() {
     let cells = document.querySelectorAll(".kmap > .cell");
-
     for (let c of cells)
-        c.addEventListener('click', () => nextValue(c));
+        c.addEventListener('click', () => c.innerHTML = nextValue(c.innerHTML));
+
+
+    let solveBtn = document.getElementById("solveBtn");
+    // solveBtn.addEventListener('click', parseKmap(Array.from(cells).map((c) => c.innerHTML)));
+    solveBtn.addEventListener('click', () => solve(cells));
+
 }
 
 main();
