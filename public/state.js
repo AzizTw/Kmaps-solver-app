3778ca8319cf1ed923a0b8725b71745b4dca1b84
AzizTw <abdulaziz742.t@gmail.com>
2022-12-07 23:02:11 +0300

    New module, state.js. Made to be used in both modes.

---
+++ public/state.js
@@ -0,0 +1,27 @@
+export class State {
+    constructor() {
+        this.kmap = document.querySelector('.kmap');
+        this.select = document.getElementById('kmapSize')
+        this.solbox = document.getElementById("solutionBox");
+
+        this.setN(parseInt(this.select.value));
+    }
+
+    kmapDim(num_vars) {
+        let rows = 2 ** Math.floor(num_vars / 2);
+        let columns = 2 ** Math.floor((num_vars + 1) / 2);
+
+        return { rows, columns };
+    }
+
+    setN(n) {
+        this.n = n;
+        let dim = this.kmapDim(this.n);
+        this.nRows = dim.rows;
+        this.nCols = dim.columns;
+    }
+
+    getCells() {
+        return this.kmap.children;
+    }
+}
