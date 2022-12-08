90a0dfd3419f1ac9a97ac29dcc707586980cefd8
AzizTw <abdulaziz742.t@gmail.com>
2022-12-08 22:09:56 +0300

    Practice.js is functional now, will implement more features to cover some cases.

--- public/practice.js
+++ public/practice.js
@@ -3,6 +3,10 @@ import { drawKmap, labelCells, getKmapInput, getSolution, VALUES, LENGTH } from 
 
 let globalSolution;
 
+let nEPIsInput = document.getElementById("nEPIs");
+let nPIsInput = document.getElementById("nPIs");
+let sopInput = document.getElementById("SOP");
+
 console.log("practice.js loaded");
 
 
@@ -30,6 +34,45 @@ function storeSolution(state) {
     });
 }
 
+function checkSOP(answerSOP){
+    for (let correctSOP of globalSolution.sops)
+        if (answerSOP.every((minterm) => correctSOP.includes(minterm)))
+            return true;
+    return false;
+}
+
+function showAnswerResult(answer, id){
+    let result = document.getElementById(id);
+    if (answer){
+        result.innerHTML = "&#10004;"
+        result.classList.add("check");
+        result.classList.remove("cross"); // remove cross class if it exists
+    }
+    else{
+        result.innerHTML = "&#10008;"
+        result.classList.add("cross");
+        result.classList.remove("check"); // remove check class if it exists
+    }    
+}
+
+function checkSolution(){
+    let nEPIs = parseInt(nEPIsInput.value);
+    let nPIs = parseInt(nPIsInput.value);
+    let sop = sopInput.value.toUpperCase();
+    //remove all spaces from sop
+    sop = sop.replace(/\s/g, '');
+    //split sop into an array of minterms
+    sop = sop.split("+");
+
+    let nEPIsCorrect = globalSolution.epis.length === nEPIs;
+    let nPIsCorrect = globalSolution.pis.length === nPIs;
+    let sopCorrect = checkSOP(sop);
+
+    showAnswerResult(nEPIsCorrect, "nPIsCheck");
+    showAnswerResult(nPIsCorrect, "nEPIsCheck");
+    showAnswerResult(sopCorrect, "SOPCheck");
+}
+
 function main() {
     let state = new State();
     drawKmap(state);
@@ -44,6 +87,10 @@ function main() {
     document.getElementById("randBtn").addEventListener("click", () => {
         randomizeKmap(state);
         storeSolution(state);
+    });
+
+    document.getElementById("checkBtn").addEventListener("click", () => {
+        checkSolution();
     });
 }
 
